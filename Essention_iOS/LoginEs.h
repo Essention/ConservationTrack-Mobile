ab78827341e63b2b594641cd9b20dad456a37a81
SerAlalal <seralalal@gmail.com>
2014-10-28 19:21:11 +0200

    w

---
+++ Essention_iOS/LoginEs.h
@@ -0,0 +1,19 @@
+//
+//  LoginEs.h
+//  Essention_iOS
+//
+//  Created by Oleksii on 10/28/14.
+//  Copyright (c) 2014 Oleksii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginEs : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *username;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+
+- (IBAction)bLogin:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *bLogin;
+
+
+@end
